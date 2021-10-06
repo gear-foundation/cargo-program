0c863b09654ee346d307028abae371f39a08de29
Shamil <shamil@shamil.ru>
2021-10-06 08:47:39 +0300

    Add a stub argument for on-chain running

--- src/commands/run.rs
+++ src/commands/run.rs
@@ -11,6 +11,7 @@ use super::BuildCommand;
 use crate::error::CrateError;
 
 const REGULAR_FEE: u32 = 1000;
+const DEFAULT_NODE_ADDRESS: &str = "127.0.0.1:9944";
 
 /// Run a Gear program off-chain or using local node
 #[derive(Clap, Clone, Debug)]
@@ -22,12 +23,12 @@ pub(crate) struct RunCommand {
     /// Path to Cargo.toml
     #[clap(long, default_value = "Cargo.toml")]
     pub(crate) manifest_path: PathBuf,
-    /// Use `wasmer` runtime for running WASM code off-chain (default)
+    /// Off-chain: Use `wasmer` runtime for running WASM code [default]
     #[clap(long)]
     wasmer: bool,
-    /// Local node's address and port
-    #[clap(long, default_value = "127.0.0.1:9944")]
-    node: String,
+    /// On-chain: Local node's address and port
+    #[clap(long)]
+    node: Option<Option<String>>,
 }
 
 impl RunCommand {
@@ -57,10 +58,22 @@ impl RunCommand {
             .map_err(|_| CrateError::UnableToInjectGasCounter(program_path.clone()))?;
         let code = elements::serialize(instrumented_module)
             .context("unable to serialize the program code")?;
-        self.run_off_chain(code)
+        if let Some(ref node) = self.node {
+            let address = node
+                .as_ref()
+                .map(String::as_str)
+                .unwrap_or(DEFAULT_NODE_ADDRESS);
+            self.run_on_chain(code, address)
+        } else {
+            self.run_off_chain(code)
+        }
     }
 
     fn run_off_chain(&self, _code: Vec<u8>) -> Result<()> {
+        Err(CrateError::UnimplementedCommand.into())
+    }
+
+    fn run_on_chain(&self, _code: Vec<u8>, _address: &str) -> Result<()> {
         Err(CrateError::UnimplementedCommand.into())
     }
 }
