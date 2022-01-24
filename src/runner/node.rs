8652c7b7b8e29792ac92a3e60ef6d67ac526210a
Shamil <shamil@shamil.ru>
2022-01-24 11:42:20 +0300

    Try to fix

--- src/runner/node.rs
+++ src/runner/node.rs
@@ -1,22 +1,20 @@
 use anyhow::Result;
-use sp_keyring::{sr25519::sr25519::Pair, AccountKeyring};
-use subxt::{ClientBuilder, PairSigner};
+use sp_keyring::AccountKeyring;
+use subxt::{ClientBuilder, DefaultConfig, DefaultExtra, PairSigner};
 use tokio::runtime::Runtime;
 
 #[subxt::subxt(runtime_metadata_path = "node-metadata.scale")]
 mod gear_node {}
-use gear_node::{DefaultConfig, RuntimeApi};
+use gear_node::RuntimeApi;
 
 pub struct Node {
     url: Option<String>,
-    signer: PairSigner<DefaultConfig, Pair>,
 }
 
 impl Node {
     pub fn new() -> Self {
         Node {
             url: None,
-            signer: Self::alice(),
         }
     }
 
@@ -25,11 +23,7 @@ impl Node {
         self
     }
 
-    fn alice() -> PairSigner<DefaultConfig, Pair> {
-        PairSigner::new(AccountKeyring::Alice.pair())
-    }
-
-    async fn api(&self) -> Result<RuntimeApi<DefaultConfig>> {
+    async fn api(&self) -> Result<RuntimeApi<DefaultConfig, DefaultExtra<DefaultConfig>>> {
         let api_builder = ClientBuilder::new();
         let api_builder = if let Some(url) = &self.url {
             api_builder.set_url(url)
@@ -39,7 +33,7 @@ impl Node {
         let api = api_builder
             .build()
             .await?
-            .to_runtime_api::<RuntimeApi<DefaultConfig>>();
+            .to_runtime_api();//::<RuntimeApi<DefaultConfig, DefaultExtra<DefaultConfig>>>();
         Ok(api)
     }
 
@@ -51,13 +45,15 @@ impl Node {
         gas_limit: u64,
         value: u128,
     ) -> Result<()> {
+        let signer = PairSigner::new(AccountKeyring::Alice.pair());
+
         let rt = Runtime::new()?;
         rt.block_on(async {
             let api = self.api().await?;
             api.tx()
                 .gear()
                 .submit_program(code, salt, init_payload, gas_limit, value)
-                .sign_and_submit(&self.signer)
+                .sign_and_submit(&signer)
                 .await?;
             Ok(())
         })
